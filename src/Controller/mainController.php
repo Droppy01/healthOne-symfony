c7229509bbdf0445877ec6678884396cbc524e96
Droppy01 <aron@dingweb.nl>
2021-01-08 12:54:34 +0100

    add addMedication Methot

--- src/Controller/mainController.php
+++ src/Controller/mainController.php
@@ -2,7 +2,10 @@
 namespace App\Controller;
 
 use App\Entity\Medication;
+
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -16,14 +19,46 @@ class mainController extends AbstractController {
     }
     
     /**
-     * @Route("/medicijnen", name="medicijnen_page")
+     * @Route("/medicijnen", name="medicijnen_page", methods={"get"})
      */
-    public function showMedicijnen() : Response   {
+    public function showMedications() : Response   {
         $medications = $this->getDoctrine()->getManager()->getRepository(Medication::class)->findAll();
-
-
-
-
         return $this->render("medicijnen.html.twig", ["medications"=>$medications] );
     }
+
+    /**
+     * @Route("/medicijnen", name="medicijn_toevoegen", methods={"post"})
+     */
+    public function addMedication(Request $request) {
+        if($request->getContentType() == "json") {
+            $data = json_decode($request->getContent(), true);
+        }
+
+        $Medication = new Medication();
+
+        if (isset($data["Price"])) {
+            $Medication->setPrice($data["Price"]);
+        }
+
+        if (isset($data['Name'])) {
+            $Medication->setName($data['Name']);
+        }
+
+        if (isset($data["PharmaceuticalCompany"])) {
+            $Medication->setPharmaceuticalCompany($data["PharmaceuticalCompany"]);
+        }
+
+        if (isset($data["Insured"])) {
+            $Medication->setInsured($data["Insured"]);
+        }
+
+        // TODO make validate request
+
+        $Manager = $this->getDoctrine()->getManager();
+        $Manager->persist($Medication);
+        $Manager->flush();
+        return new JsonResponse($data);
+    }
+
+
 }
